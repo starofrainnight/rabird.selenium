52b4e5c0d99d48f3dd7c0d686b0b7ef59ca5e5b7
Hong-She Liang <starofrainnight@gmail.com>
2015-03-16 10:47:40 +0800

    Initial commit
    Extract selenium module from rabird.auto. I think it should be a standalone module.

---
+++ src/rabird/selenium/screen.py
@@ -0,0 +1,70 @@
+
+
+
+'''
+@date 2013-12-17
+
+@author Hong-She Liang <starofrainnight@gmail.com>
+'''
+
+import cv2
+import sys
+import numpy
+
+if sys.platform == "win32":
+	import ImageGrab
+else:
+	import pyscreenshot as ImageGrab
+
+class ImageLookupError(LookupError):
+	pass
+
+def find(target_image, threshold=0.7, is_center_position=True):
+	'''
+	Find a specific image from screen
+	
+	@arg [in] target_image Target image we want to find, if input value type is 
+	'str', this method will open an image by cv2.imread(). Otherwise, the 
+	target_image type should be an image stored in numpy.array.
+	
+	@arg [in] is_center_position If True, the result point indicate the center
+	point of found image, otherwise the left-top point will be return. 
+	
+	@return If successed, the location of specific image on screen will 
+	return, otherwise None will be return. The return position could control 
+	by argument 'is_center_position'.
+	'''
+
+	match_method = cv2.TM_SQDIFF_NORMED
+	screen_image = ImageGrab.grab()
+
+	# R and B reversed !
+	screen_image = numpy.array(screen_image)
+	screen_image = cv2.cvtColor(screen_image, cv2.COLOR_RGB2BGR)
+	
+	if str==type(target_image):
+		template_image = cv2.imread(target_image)
+	else:
+		template_image = target_image 
+
+	result = cv2.matchTemplate(screen_image, template_image, match_method)
+	min_value, max_value, min_location, max_location = cv2.minMaxLoc(result)
+
+	# For SQDIFF and SQDIFF_NORMED, the best matches are lower values. For all the other methods, the higher the better
+	if ( match_method  == cv2.TM_SQDIFF ) or ( match_method == cv2.TM_SQDIFF_NORMED ):
+		match_location = min_location
+		match_value = 1.0 - min_value
+	else:
+		match_location = max_location
+		match_value = max_value
+		
+	if match_value < threshold:
+		raise ImageLookupError('Target image can\'t not found on screen !')
+		
+	if is_center_position:
+		match_location = (
+			match_location[0] + template_image.shape[1] // 2,
+			match_location[1] + template_image.shape[0] // 2 )
+		
+	return match_location
+	
