e3737dc7ba9413e377f4e330c245d8b0c4a5adf6
Hong-She Liang <starofrainnight@gmail.com>
2015-04-21 14:45:48 +0800

    Follow recent rabird.core's setup process changes
    Generate long_description by ourself
    Added CHANGES.rst for record major changes during each version
    Ignore all dynamic loadable libraries and generated objects
    Included all *.txt and *.rst (just like CHANGES.rst)
    Renamed LICENSE to LICENSE.txt, I think a suffix ".txt" is friendly while targeting cross-platform
    Fixed pip can't find us

---
+++ README.rst
@@ -0,0 +1,3 @@
+rabird.selenium
+---------------
+
--- setup.py
+++ setup.py
@@ -10,44 +10,47 @@ import shutil
 import logging
 import fnmatch
 import rabird.core.distutils
-import rabird.core.logging
 from setuptools import setup, find_packages
 
-from_package = 'src'
-to_package = 'rabird'
 package_name = 'rabird.selenium'
 
-rabird.core.logging.load_default_config()
-
 # Convert source to v2.x if we are using python 2.x.
-rabird.core.distutils.preprocess_sources_for_compatible(from_package, os.path.realpath(os.curdir))
+source_dir = rabird.core.distutils.preprocess_source()
 
 # Exclude the original source package, only accept the preprocessed package!
-our_packages = find_packages(exclude=[from_package, '%s.*' % (from_package)])
+our_packages = find_packages(where=source_dir)
 
 our_requires = [
 	'selenium',
-	'webdriverplus',
 	]
+
+long_description=(
+     open('README.rst', 'r').read()
+     + '\n' +
+     open('CHANGES.rst', 'r').read()
+     )
 	
 setup(
 	name=package_name,
-	version='.'.join(map(str, (0, 3, 0))),
+	version='.'.join(map(str, (0, 3, 1))),
 	author='Hong-She Liang',
 	author_email='starofrainnight@gmail.com',
-	url='',
-	py_modules=[to_package],
+	url='https://github.com/starofrainnight/%s' % package_name,
 	description='%s utilities' % package_name,
+	long_description=long_description,
 	classifiers=[
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 3',
-		'Intended Audience :: Developers',
-		'License :: OSI Approved :: MIT License',
-		'Topic :: Software Development :: Libraries',
-		'Topic :: Utilities',
-	],
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha', 
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries',
+    ],
 	install_requires=our_requires,
+	package_dir = {'': source_dir},
     packages=our_packages,
-    namespace_packages = ['rabird'],
+    namespace_packages=[package_name.split(".")[0]],
+    # If we don't set the zip_safe to False, pip can't find us.
+    zip_safe=False,
 	)
 
