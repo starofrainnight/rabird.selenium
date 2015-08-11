1b0a1aa4bfe048f1e31131e7c8e6cc4709670bc8
Hong-She Liang <starofrainnight@gmail.com>
2015-08-11 16:18:28 +0800

    Added easily expected conditions : xpath_find() and xpath_find_all()

--- src/rabird/selenium/expected_conditions.py
+++ src/rabird/selenium/expected_conditions.py
@@ -53,3 +53,39 @@ class eecf_enable_of(object):
         # Calling any method forces a staleness check
         return element.is_enabled() == self.__check_status
         
+class xpath_find(object):
+    def __init__(self, xpath_expr, expected_conditions=[]):
+        self.__xpath_expr = xpath_expr
+        self.__expected_conditions = expected_conditions
+
+    def __call__(self, driver):
+        elements = driver.xpath_find(self.__xpath_expr, 
+            expected_conditions=self.__expected_conditions)
+        if len(elements) > 0:
+            return elements
+        else:
+            return False
+        
+class xpath_find_all(object):
+    """
+    Easily to wait until find all elements recursively.
+    
+    Example : 
+    
+    @code
+    elements = WebDriverWait(driver, 10).until(EC.xpath_find(
+        "//input[@id='fm-login-id']",
+        expected_conditions=[eecf_visible_of(True)]))
+    @endcode 
+    """
+    def __init__(self, xpath_expr, expected_conditions=[]):
+        self.__xpath_expr = xpath_expr
+        self.__expected_conditions = expected_conditions
+
+    def __call__(self, driver):
+        elements = driver.xpath_find_all(self.__xpath_expr, 
+            expected_conditions=self.__expected_conditions)
+        if len(elements) > 0:
+            return elements
+        else:
+            return False
