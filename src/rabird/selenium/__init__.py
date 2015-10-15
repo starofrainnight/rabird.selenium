b7ab1437de0e436f31c7ad8e324e6f1d510bfd98
Hong-She Liang <starofrainnight@gmail.com>
2015-10-15 16:19:23 +0800

    Fixed differences of types.MethodType between python2 and python3

--- src/rabird/selenium/__init__.py
+++ src/rabird/selenium/__init__.py
@@ -7,6 +7,7 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from . import webelement
 from . import webdriver 
 import types
+import six
 
 __is_monkey_patched = False
 
@@ -18,36 +19,36 @@ def monkey_patch():
 
     __is_monkey_patched = True
         
-    WebElement.set_attribute = types.MethodType(webelement.set_attribute, None, WebElement)
-    WebElement.force_focus = types.MethodType(webelement.force_focus, None, WebElement)
+    WebElement.set_attribute = six.create_bound_method(webelement.set_attribute, WebElement)
+    WebElement.force_focus = six.create_bound_method(webelement.force_focus, WebElement)
     # Do not try to override the WebElement.click(), that's a different method, 
     # it will scroll to the element then click the element, if we use 
     # force_click() instead, it will take no effect on <option>. 
-    WebElement.force_click = types.MethodType(webelement.force_click, None, WebElement)
-    WebElement.force_hover = types.MethodType(webelement.force_hover, None, WebElement)
+    WebElement.force_click = six.create_bound_method(webelement.force_click, WebElement)
+    WebElement.force_hover = six.create_bound_method(webelement.force_hover, WebElement)
     
     WebElement._old_execute = WebElement._execute 
-    WebElement._execute = types.MethodType(webelement._execute, None, WebElement)
+    WebElement._execute = six.create_bound_method(webelement._execute, WebElement)
 
-    WebElement.find_element_recursively = types.MethodType(webelement.find_element_recursively, None, WebElement)
+    WebElement.find_element_recursively = six.create_bound_method(webelement.find_element_recursively, WebElement)
     
-    WebElement.xpath_find = types.MethodType(webelement.xpath_find, None, WebElement)
-    WebElement.xpath_find_all = types.MethodType(webelement.xpath_find_all, None, WebElement)
+    WebElement.xpath_find = six.create_bound_method(webelement.xpath_find, WebElement)
+    WebElement.xpath_find_all = six.create_bound_method(webelement.xpath_find_all, WebElement)
     
     WebDriver._old_switch_to_frame = WebDriver.switch_to_frame
-    WebDriver.switch_to_frame = types.MethodType(webdriver.switch_to_frame, None, WebDriver)
-    WebDriver.force_get = types.MethodType(webdriver.force_get, None, WebDriver)
+    WebDriver.switch_to_frame = six.create_bound_method(webdriver.switch_to_frame, WebDriver)
+    WebDriver.force_get = six.create_bound_method(webdriver.force_get, WebDriver)
     
-    WebDriver.find_element_recursively = types.MethodType(webelement.find_element_recursively, None, WebDriver)
+    WebDriver.find_element_recursively = six.create_bound_method(webelement.find_element_recursively, WebDriver)
     
-    WebDriver.xpath_find = types.MethodType(webelement.xpath_find, None, WebDriver)
-    WebDriver.xpath_find_all = types.MethodType(webelement.xpath_find_all, None, WebDriver)
+    WebDriver.xpath_find = six.create_bound_method(webelement.xpath_find, WebDriver)
+    WebDriver.xpath_find_all = six.create_bound_method(webelement.xpath_find_all, WebDriver)
     
-    WebDriver.xpath_wait = types.MethodType(webelement.xpath_wait, None, WebDriver)
-    WebDriver.xpath_wait_all = types.MethodType(webelement.xpath_wait_all, None, WebDriver)
+    WebDriver.xpath_wait = six.create_bound_method(webelement.xpath_wait, WebDriver)
+    WebDriver.xpath_wait_all = six.create_bound_method(webelement.xpath_wait_all, WebDriver)
         
-    WebDriver.get_xpath_wait_timeout = types.MethodType(webdriver.get_xpath_wait_timeout, None, WebDriver)
-    WebDriver.set_xpath_wait_timeout = types.MethodType(webdriver.set_xpath_wait_timeout, None, WebDriver)
+    WebDriver.get_xpath_wait_timeout = six.create_bound_method(webdriver.get_xpath_wait_timeout, WebDriver)
+    WebDriver.set_xpath_wait_timeout = six.create_bound_method(webdriver.set_xpath_wait_timeout, WebDriver)
     
 # Try to do the monkey patch while importing this module
 monkey_patch()
