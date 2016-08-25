aab11883f75e76511f60f61e8be4c5b89dcefebd
Hong-She Liang <starofrainnight@gmail.com>
2016-08-25 17:52:32 +0800

    Bump version to 0.6.1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ long_description = (
 
 setup(
     name=package_name,
-    version='.'.join(map(str, (0, 6, 0))),
+    version='.'.join(map(str, (0, 6, 1))),
     author='Hong-She Liang',
     author_email='starofrainnight@gmail.com',
     url='https://github.com/starofrainnight/%s' % package_name,
