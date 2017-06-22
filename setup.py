7d84fcd484ef90e9b70a984cac4ccb60d1981b33
Hong-She Liang <starofrainnight@gmail.com>
2017-06-22 22:38:46 +0800

    Bump version: 0.7.1 → 0.7.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ long_description = (
 
 setup(
     name=package_name,
-    version='0.7.1',
+    version='0.7.2',
     author='Hong-She Liang',
     author_email='starofrainnight@gmail.com',
     url='https://github.com/starofrainnight/%s' % package_name,
