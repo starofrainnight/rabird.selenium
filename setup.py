f81c71213ba24311b7a03b894a22cdfc4b586acc
Hong-She Liang <starofrainnight@gmail.com>
2017-11-02 18:20:56 +0800

    Removed python2 support

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Software Development :: Libraries",
