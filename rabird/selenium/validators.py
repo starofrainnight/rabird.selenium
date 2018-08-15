2141e71e35c67218eb1a2f7249b58b1a547b68c2
Hong-She Liang <starofrainnight@gmail.com>
2018-08-15 21:21:15 +0800

    Other exceptions must re-raise!
    Reverted changes at 901134efb139dbe068067b065f89639f47f59e82

--- rabird/selenium/validators.py
+++ rabird/selenium/validators.py
@@ -87,9 +87,6 @@ class VisibleAny(VisibleOf):
             # These exceptions are means element disapperred.
             # So it's not in any visible status!
             return False
-        else:
-            # Other exceptions are means it's valid !
-            return True
 
 
 class Visible(VisibleOf):
